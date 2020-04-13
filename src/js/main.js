764551ba0aff0328e355bcb1f21f09e24902d115
Sam Dean <sam.dean@substrakt.com>
2020-04-13 13:19:51 +0100

    UPDATE: code to use vue

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,4 @@
 const axios = require('axios');
-const handlebars = require('handlebars');
 const moment = require('moment');
 
 // https://services-api.ryanair.com/farfnd/3/oneWayFares?&departureAirportIataCode=BHX&language=en&limit=16&market=en-gb&offset=0&outboundDepartureDateFrom=2019-02-11&outboundDepartureDateTo=2019-10-28&priceValueTo=150
@@ -9,40 +8,66 @@ const dateFormat = 'YYYY-MM-DD';
 const today = moment().format(dateFormat);
 const endDate = moment().add(3, 'months').format(dateFormat);
 
-handlebars.registerHelper('formatDate', function(dateString) {
-    return new handlebars.SafeString(
-        moment(dateString).format("DD-MM-YYYY HH:mm")
-    );
-});
 
-const select = document.getElementById('airportIATACode');
+var app = new Vue({
+    el: '#root',
+    data: {
+        fares: null,
+        loading: false,
+    },
+    methods: {
+        getFares(event) {
 
-select.addEventListener('change', function() {
-    axios.get(baseUrl, {
-        params: {
-            language: 'en',
-            offset: 0,
-            departureAirportIataCode: this.value,
-            limit: 36,
-            market: 'en-gb',
-            outboundDepartureDateFrom: today,
-            outboundDepartureDateTo: endDate,
-            priceValueTo: '150',
+            this.loading = true,
+
+            axios
+            .get(baseUrl, {
+                params: {
+                    language: 'en',
+                    offset: 0,
+                    departureAirportIataCode: event.target.value,
+                    limit: 36,
+                    market: 'en-gb',
+                    outboundDepartureDateFrom: today,
+                    outboundDepartureDateTo: endDate,
+                    priceValueTo: '150',
+                }
+            })
+            .then(response => (
+                this.fares = response.data.fares,
+                console.log(response.data.fares)
+            ))
+            .catch(error => console.log(error))
+            .finally(() => this.loading = false)
         }
-    })
-    .then(function (response) {
-        console.log(response.data.fares);
-        let data = response.data;
-        let target = document.getElementById('target');
-        let source = document.getElementById('template').innerHTML;
-        let template = handlebars.compile(source);
-
-        target.innerHTML = template(data);
-    })
-    .catch(function (error) {
-        console.log(error);
-    })
-    .finally(function () {
-        // always executed
-    });
+    }
 });
+
+
+new Vue({
+    el: '#app',
+    data () {
+      return {
+        info: null,
+        loading: true,
+        errored: false
+      }
+    },
+    filters: {
+      currencydecimal (value) {
+        return value.toFixed(2)
+      }
+    },
+    mounted () {
+      axios
+        .get('https://api.coindesk.com/v1/bpi/currentprice.json')
+        .then(response => {
+          this.info = response.data.bpi
+        })
+        .catch(error => {
+          console.log(error)
+          this.errored = true
+        })
+        .finally(() => this.loading = false)
+    }
+  })
